5bf1827e81a4f509e3ef101efbeae9e88a85a95a
A-Danylevych <anton231255@gmail.com>
2021-10-02 17:15:56 +0300

    Cell first version

---
+++ Models/Cell.cs
@@ -0,0 +1,10 @@
+namespace Model
+{
+    class Cell
+    {
+        Cell LeftCell { get; set; }
+        Cell RightCell { get; set; }
+        Cell UpCell { get; set; }
+        Cell DownCell { get; set; }
+    }
+}
