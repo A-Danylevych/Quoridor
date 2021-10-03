377b388bcace46a4421a018b85adc8ca313a735f
A-Danylevych <anton231255@gmail.com>
2021-10-03 18:50:37 +0300

    gameStatefixin

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Model
 {
     public class Game
@@ -5,14 +7,21 @@ namespace Model
         private Player TopPlayer;
         private Player BottomPlayer;
         private Player CurrentPlayer;
+        private GameState gameState;
         private Game instance;
-        private bool isRunning; 
         private Board Board;
         private static object syncRoot = new Object(); 
         public Game()
         {
             isRunning = true;
             Board = Board.GetInstance();
+            Cell topStartPosition = Board.TopStartPosition();
+            TopPlayer = new Player(topStartPosition);
+            Cell bottomStartPosition = Board.BottomStartPosition();
+            BottomPlayer = new Player(bottomStartPosition);
+            List<Cell> topWinningCells = Board.TopWinningCells();
+            List<Cell> bottomWinningCells = Board.BottomWinningCells();
+            gameState = new GameState(topWinningCells, bottomWinningCells);
         }
         public void ChangeCurrentPlayer()
         {
@@ -22,7 +31,7 @@ namespace Model
             }
             else
             {
-                CurrentPlayer = Bottomlayer;
+                CurrentPlayer = BottomPlayer;
             }
         }
         public void Update()
--- Models/GameState.cs
+++ Models/GameState.cs
@@ -4,19 +4,29 @@ namespace Model
 {
     class GameState
     {
-        bool inPlay;
-        bool TopWon;
-        bool BottomWon;
-        public bool CheckWinning(Cell currentCell, List<Cell> winningCells)
+        public bool inPlay {get; private set;}
+        List<Cell> TopWinningCells;
+        List<Cell> BottomWinningCells;
+        public bool CheckTopWinning(Player player) => CheckWinning(player.CurrentCell, TopWinningCells);
+        public bool CheckBottompWinning(Player player) => CheckWinning(player.CurrentCell, BottomWinningCells);
+
+        private bool CheckWinning(Cell currentCell, List<Cell> winningCells)
         {
             foreach(var cell in winningCells)
             {
                 if(currentCell == cell)
                 {
+                    inPlay = false;
                     return true;
                 }
             }
             return false;
+        }
+        public GameState(List<Cell> topWinningCells, List<Cell> bottomWinningCells)
+        {
+            TopWinningCells = topWinningCells;
+            BottomWinningCells = bottomWinningCells;
+            inPlay = true;
         }
     }
 }
