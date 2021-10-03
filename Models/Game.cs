e2499edcaf8f54e0d4dd721506fabefb0a6a7a71
A-Danylevych <anton231255@gmail.com>
2021-10-03 19:08:11 +0300

    game logic first version

--- Models/Game.cs
+++ Models/Game.cs
@@ -5,20 +5,26 @@ namespace Model
     public class Game
     {
         private Player TopPlayer;
+        public IController Controller {get; private set;}
+        public IViewer Viewer {get; private set;}
         private Player BottomPlayer;
         private Player CurrentPlayer;
         private GameState gameState;
         private Game instance;
         private Board Board;
         private static object syncRoot = new Object(); 
-        public Game()
+        public Game(IController controller, IViewer viewer)
         {
+            Controller = controller;
+            viewer = viewer;
             isRunning = true;
             Board = Board.GetInstance();
+            Board.NewBoard();
             Cell topStartPosition = Board.TopStartPosition();
             TopPlayer = new Player(topStartPosition);
             Cell bottomStartPosition = Board.BottomStartPosition();
             BottomPlayer = new Player(bottomStartPosition);
+            CurrentPlayer = BottomPlayer;
             List<Cell> topWinningCells = Board.TopWinningCells();
             List<Cell> bottomWinningCells = Board.BottomWinningCells();
             gameState = new GameState(topWinningCells, bottomWinningCells);
@@ -36,6 +42,15 @@ namespace Model
         }
         public void Update()
         {
+            switch(Controller.WaitForAction)
+            {
+                case Action.MakeMove:
+
+                        Viewer.RenderPossibleCellsToMove();             
+                        break;
+                case Action.PlaceWall:
+                    break;
+            }
             ChangeCurrentPlayer();
         }
         public static Game GetInstance()
