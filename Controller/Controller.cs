a6e63fc3b955338d2474880c093c598879b61a37
A-Danylevych <anton231255@gmail.com>
2021-10-06 16:22:00 +0300

    Controlle and view base classes

---
+++ Controller/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Quoridor_1._0.Controller
+{
+    class Controller : IController
+    {
+        public Model.Action WaitForAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cell WaitForCell()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Wall WaitForWall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ View/View.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Quoridor_1._0.View
+{
+    class View : IViewer
+    {
+        public void ChangePlayer(string winner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderEnding()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderPlayer(int top, int left)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderRemainingWalls(int TopCount, int BottomCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderWall(int top, int left)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
